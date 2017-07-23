8fe4c381c30f30eb58bfc9753affdfd44f832c47
Zdeněk Papučík <zdenek.papucik@gmail.com>
2017-07-23 15:25:42 +0200

    Update Sessions.php

--- src/Drago/Http/Sessions.php
+++ src/Drago/Http/Sessions.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Extending for Nette Framework.
+ * Extending for Nette Framework
  * Copyright (c) 2015, Zdeněk Papučík
  */
 namespace Drago\Http;
@@ -42,7 +42,7 @@ class Sessions
 	}
 
 	/**
-	 * @param  string
+	 * @param string
 	 * @return Http\SessionSection
 	 */
 	public function getSessionSection()
