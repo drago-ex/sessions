2e4cb47464d3dd13db4a65b251f8773acc4dfe71
accgit <zdenek.papucik@gmail.com>
2019-09-27 10:50:23 +0200

    Update

---
+++ tests/Sessions.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+use Drago\Http\Sessions;
+use Nette\Http\RequestFactory;
+use Nette\Http\Response;
+use Nette\Http\Session;
+use Tester\Assert;
+
+require __DIR__ . '/bootstrap.php';
+
+$request = (new RequestFactory())->createHttpRequest();
+$session = new Sessions(new Session($request, new Response()), 'sessions');
+
+$sessionSection = $session->getSessionSection();
+$sessionSection->value = 'Value';
+
+Assert::same('Value', $sessionSection->value);
+
+$sessionSection->next = 'Next';
+Assert::same('Next', $sessionSection->next);
+
+$sessionSection->array = [
+	'Test 1', 'Test 2'
+];
+
+Assert::same('Test 1', $sessionSection->array[0]);
+Assert::same('Test 2', $sessionSection->array[1]);
