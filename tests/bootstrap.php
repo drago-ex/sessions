ca7fd51b1afd7b470b34aecab750c0e90243450f
accgit <zdenek.papucik@gmail.com>
2020-06-15 09:20:33 +0200

    Update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,9 @@
 
 declare(strict_types = 1);
 
+use Tester\Environment;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-Tester\Environment::setup();
+Environment::setup();
 date_default_timezone_set('Europe/Prague');
